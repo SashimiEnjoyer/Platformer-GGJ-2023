6b2be8ded06e586c79a59698302d784aa6a23dbe
CaptainWatanabe <farispradiatma77@gmail.com>
2023-02-03 18:39:59 +0700

    Add season manager script
    + add text mesh pro

--- Assets/Scripts/SeasonManager.cs
+++ Assets/Scripts/SeasonManager.cs
@@ -1,14 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Threading;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public enum Season { Winter, Spring, Summer, Autumn}
 
 public class SeasonManager : MonoBehaviour
 {
-
     public static SeasonManager instance;
-    public Season season;
+    public TMP_Text testtimer;
+    
+    private Season _season;
+    private float seasonTimer;
+    [SerializeField] private float seasonCooldown;
+    public UnityAction<Season> onSeasonChange;
+    public Season season
+    {
+        set
+        {
+            if (value == _season)
+                return;
+
+            _season = value;
+            onSeasonChange?.Invoke(value);
+            Debug.Log("Set Season: " + _season);
+        }
+
+        get
+        {
+            return _season;
+        }
+    }
 
     private void Awake()
     {
@@ -16,5 +38,14 @@ public class SeasonManager : MonoBehaviour
             instance = this;
         else
             Destroy(gameObject);
+    }
+
+    public void Update()
+    {
+        seasonTimer += Time.deltaTime;
+        seasonTimer %= seasonCooldown;
+
+        testtimer.text = seasonTimer.ToString();
+
     }
 }
