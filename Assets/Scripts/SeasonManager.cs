dfefc221a47b53325011cafb36ce0a64903b1d76
CaptainWatanabe <farispradiatma77@gmail.com>
2023-02-03 18:24:10 +0700

    Tilemap
    - Add tilemap assets
    - Add tilemap editor package

---
+++ Assets/Scripts/SeasonManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Season { Winter, Spring, Summer, Autumn}
+
+public class SeasonManager : MonoBehaviour
+{
+
+    public static SeasonManager instance;
+    public Season season;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+}
