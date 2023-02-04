8157441a11f03eeed8e224c77d86e00e8b126911
CaptainWatanabe <farispradiatma77@gmail.com>
2023-02-04 11:41:39 +0700

    Proper change season by only set variable

--- Assets/Scripts/SeasonManager.cs
+++ Assets/Scripts/SeasonManager.cs
@@ -3,6 +3,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
+[Serializable]
 public enum Season { Winter, Spring, Summer, Autumn}
 
 public class SeasonManager : MonoBehaviour
@@ -11,9 +12,8 @@ public class SeasonManager : MonoBehaviour
     public TMP_Text testtimer;
     public TMP_Text seasonText;
     
-    private Season _season;
+    [SerializeField]private Season _season;
     private float seasonTimer;
-    private float divValue;
     public float timerMultiplier = 1;
     [SerializeField] private float seasonCooldown;
     public UnityAction<Season> onSeasonChange;
@@ -25,6 +25,7 @@ public class SeasonManager : MonoBehaviour
                 return;
 
             _season = value;
+            seasonTimer = seasonCooldown * ((float)_season / Enum.GetNames(typeof(Season)).Length);
             onSeasonChange?.Invoke(value);
             Debug.Log("Set Season: " + _season);
         }
@@ -55,21 +56,39 @@ public class SeasonManager : MonoBehaviour
 
     private void Start()
     {
-        divValue = seasonCooldown / Enum.GetNames(typeof(Season)).Length;
+        season = Season.Winter;
     }
 
     public void Update()
     {
+
+        if (seasonTimer > 0 && seasonTimer < 4)
+            season = Season.Winter;
+        else if (seasonTimer > 4 && seasonTimer < 8)
+            season = Season.Spring;
+        else if (seasonTimer > 8 && seasonTimer < 12)
+            season = Season.Summer;
+        else if (seasonTimer > 12 && seasonTimer < 16)
+            season = Season.Autumn;
+
+
         seasonTimer += (Time.deltaTime * timerMultiplier);
         seasonTimer %= seasonCooldown;
 
         testtimer.text = seasonTimer.ToString();
 
-        onSeasonChange?.Invoke((Season)(seasonTimer / divValue));
+
     }
 
     void ChangeSeasonUI(Season season)
     {
         seasonText.text = season.ToString();
+    }
+
+    [ContextMenu("Test Change Season")]
+    public void ChangeSeason()
+    {
+        season = Season.Summer;
+
     }
 }
