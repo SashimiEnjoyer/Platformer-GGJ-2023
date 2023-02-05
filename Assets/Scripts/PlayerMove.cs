8cdedd569b2416d511c5fb35b5224059ca427176
Handyg11 <handygoandy@gmail.com>
2023-02-05 16:15:34 +0700

    updated animation

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -14,7 +14,7 @@ public class PlayerMove : MonoBehaviour
     private bool doubleJump, isTouching;
     Collision2D objCollider2D;
 
-    int season = 0; //1 = Fall, 2 = Winter, 3 = Spring, 4 = Summer
+    int season = 0; //1 = Fall, 2 = Winter & Summer , 3 = Spring
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -59,30 +59,6 @@ public class PlayerMove : MonoBehaviour
         {
             objCollider2D.gameObject.GetComponent<GrabableScript>().ExecuteInteractable(transform);
         }
-        //if (Input.GetKeyDown("f"))
-        //{
-        //    season = 1;
-        //    transform.localScale = new Vector3(0.6f, 0.6f, 0);
-        //    seasonTxt.text = season.ToString("F0");
-        //}
-        //if (Input.GetKeyDown("g"))
-        //{
-        //    season = 2;
-        //    transform.localScale = new Vector3(1f, 1f, 0);
-        //    seasonTxt.text = season.ToString("F0");
-        //}
-        //if (Input.GetKeyDown("h"))
-        //{
-        //    season = 3;
-        //    transform.localScale = new Vector3(2f, 2f, 0);
-        //    seasonTxt.text = season.ToString("F0");
-        //}
-        //if (Input.GetKeyDown("j"))
-        //{
-        //    season = 4;
-        //    transform.localScale = new Vector3(1f, 1f, 0);
-        //    seasonTxt.text = season.ToString("F0");
-        //}
 
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
