73372ebd3554fcb2a83747ff503ed04398d7f61d
owin40 <116938413+owin40@users.noreply.github.com>
2023-02-05 13:20:52 +0700

    Update Main Menu
    Exit & Play button

---
+++ Assets/Cainos/UI & Logo/Scenemanager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenemanager : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
