5a6e61a51befbda0162805dd82d8cb856d745805
Sashimi Enjoyer <farispradiatma77@gmail.com>
2023-02-15 23:45:48 +0700

    Add popup ui for interactable items

---
+++ Assets/Scripts/ButtonPressedInteraction.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonPressedInteraction : MonoBehaviour
+{
+    [SerializeField] UnityEvent onPressedButton;
+    bool isPressed = false;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Touch");
+
+        if (collision.CompareTag("Player") && Input.GetKeyDown(KeyCode.E) && !isPressed)
+        {
+            onPressedButton?.Invoke();
+            isPressed = true;
+        }
+    }
+}
--- Assets/Scripts/GrabableScript.cs
+++ Assets/Scripts/GrabableScript.cs
@@ -5,8 +5,47 @@ using UnityEngine;
 public class GrabableScript : MonoBehaviour
 {
     private bool isGrabbed = false;
+    [SerializeField] GameObject popup;
+    Vector2 posCache;
+
+    private void Start()
+    {
+        SeasonManager.instance.onSeasonChange += ShowPopupUI; 
+    }
+
+    private void OnDisable()
+    {
+        SeasonManager.instance.onSeasonChange -= ShowPopupUI;
+    }
+
+    private void Update()
+    {
+        if (!popup.activeInHierarchy)
+            return;
+
+        posCache = popup.transform.position;
+        posCache.x = transform.position.x;
+        popup.transform.position = posCache;
+    }
+
     public void ExecuteInteractable(Transform player, bool isGrabbed)
     {
         transform.parent = isGrabbed ? player : null;
+    }
+
+    void ShowPopupUI(Season season)
+    {
+        switch (season)
+        {
+
+            case Season.Spring:
+                popup.SetActive(true);
+                break;
+            case Season.Summer:
+                popup.SetActive(false);
+                break;
+            default:
+                break;
+        }
     }
 }
